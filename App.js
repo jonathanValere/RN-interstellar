4b121bb91ceaf37df4f43a76f34f4e4da0d0cc0c
“Jonathan <jonathan.valere@gmail.com>
2024-02-21 14:36:13 -0300

    notes part added

--- App.js
+++ App.js
@@ -1,13 +1,16 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
+import { FontAwesome } from "@expo/vector-icons";
 
-// Custom
+// Custom ----
 const marginLeftAndRightCustom = 10;
 const paddingTopAndBottomCustom = 10;
 const primaryColor = "#212121";
 const secondaryColor = "#BCBCBC";
 const thirdColor = "#fff";
 const colorBlue = "#0177BD";
+const colorYellow = "#E6B920";
+// ----
 
 export default function App() {
   const onPress = () => {
@@ -50,6 +53,47 @@ export default function App() {
               </TouchableOpacity>
             </View>
           </View>
+        </View>
+      </View>
+      <View style={styles.sectionNote}>
+        <View style={styles.blocNote}>
+          <FontAwesome
+            name="star"
+            size={27}
+            color={colorYellow}
+            style={stylesIcon.marginTopAndBottom}
+          />
+          <Text style={[stylesText.thirdColor, stylesText.middle]}>
+            <Text style={[stylesText.bold, stylesText.large]}>8,6</Text>/10
+          </Text>
+          <Text style={[stylesText.middle, stylesText.secondaryColor]}>
+            1,1m
+          </Text>
+        </View>
+        <View style={styles.blocNote}>
+          <FontAwesome
+            name="star-o"
+            size={27}
+            color={thirdColor}
+            style={stylesIcon.marginTopAndBottom}
+          />
+
+          <Text
+            style={[stylesText.bold, stylesText.large, stylesText.thirdColor]}
+          >
+            Rate this
+          </Text>
+        </View>
+        <View style={styles.blocNote}>
+          <Text style={[styles.metascore, stylesText.thirdColor]}>75</Text>
+          <Text
+            style={[stylesText.thirdColor, stylesText.middle, stylesText.bold]}
+          >
+            Metascore
+          </Text>
+          <Text style={[stylesText.middle, stylesText.secondaryColor]}>
+            46 critics reviews
+          </Text>
         </View>
       </View>
       <StatusBar style="auto" />
@@ -127,5 +171,66 @@ const styles = StyleSheet.create({
     textTransform: "uppercase",
     fontWeight: "bold",
     fontSize: 14,
+  },
+  sectionNote: {
+    flexDirection: "row",
+  },
+  blocNote: {
+    backgroundColor: primaryColor,
+    justifyContent: "top",
+    alignItems: "center",
+    paddingTop: 20,
+    paddingBottom: 15,
+    flex: 1,
+  },
+  colorText: {
+    color: thirdColor,
+  },
+  metascore: {
+    paddingTop: 3,
+    paddingBottom: 3,
+    paddingRight: 4,
+    paddingLeft: 4,
+    backgroundColor: "#62C74E",
+    fontSize: 15,
+    marginBottom: 10,
+  },
+});
+
+const stylesText = StyleSheet.create({
+  primaryColor: {
+    color: primaryColor,
+  },
+  secondaryColor: {
+    color: secondaryColor,
+  },
+  thirdColor: {
+    color: thirdColor,
+  },
+  bold: {
+    fontWeight: "bold",
+  },
+  large: {
+    fontSize: 15,
+  },
+  middle: {
+    fontSize: 12,
+  },
+  paddingTopAndBottom: {
+    paddingTop: paddingTopAndBottomCustom,
+    paddingBottom: paddingTopAndBottomCustom,
+  },
+});
+
+const stylesIcon = StyleSheet.create({
+  primaryColor: {
+    color: colorYellow,
+  },
+  marginTopAndBottom: {
+    marginBottom: 10,
+  },
+  paddingTopAndBottom: {
+    paddingTop: paddingTopAndBottomCustom,
+    paddingBottom: paddingTopAndBottomCustom,
   },
 });
